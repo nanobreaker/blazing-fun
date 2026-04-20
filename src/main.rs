a06fe16b8d73b0b058eff6bade158a11aefffe2b
nanobreaker <nan0br3aker@gmail.com>
2026-04-20 19:22:51 +0300

    add peripherals and their base tasks implementations

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,61 @@ mod pins;
 
 use ariel_os::{
     gpio::{Input, Pull},
-    log::info,
+    hal,
+    log::{error, info},
 };
 
-#[ariel_os::task(autostart, peripherals)]
-async fn main(peripherals: pins::Peripherals) {
-    info!(
-        "Started blazing-fan, running on a {} board.",
-        ariel_os::buildinfo::BOARD
-    );
+use crate::pins::{ButtonPeripherals, UartAPeripherals, UartBPeripherals};
 
-    let mut button = Input::builder(peripherals.btn, Pull::Up)
+use embedded_io_async::{Read as _, Write as _};
+
+#[ariel_os::task(autostart, peripherals)]
+async fn button_listener(peripherals: ButtonPeripherals) {
+    let mut button = Input::builder(peripherals.button, Pull::Up)
         .build_with_interrupt()
-        .expect("button should be present");
+        .expect("BUTTON on PIN_12 should be present");
 
     loop {
         button.wait_for_any_edge().await;
         info!("Button state: {}", button.is_low());
     }
+}
+
+#[ariel_os::task(autostart, peripherals)]
+async fn uart_a_listener(peripherals: UartAPeripherals) {
+    let uart_0_config = hal::uart::Config::default();
+    let mut uart_0_rx_buf = [0u8; 32];
+    let mut uart_0_tx_buf = [0u8; 32];
+    let mut uart_a = pins::UartA::new(
+        peripherals.uart0_rx,
+        peripherals.uart0_tx,
+        &mut uart_0_rx_buf,
+        &mut uart_0_tx_buf,
+        uart_0_config,
+    )
+    .expect("UART0 should be present");
+
+    loop {
+        let mut buf = [0u8; 2];
+        if let Ok(()) = uart_a.read_exact(&mut buf).await {
+            info!("bytes from uart_a");
+        } else {
+            error!("failed to read from uart_a");
+        }
+    }
+}
+
+#[ariel_os::task(autostart, peripherals)]
+async fn uart_b_listener(peripherals: UartBPeripherals) {
+    let uart_1_config = hal::uart::Config::default();
+    let mut uart_1_rx_buf = [0u8; 32];
+    let mut uart_1_tx_buf = [0u8; 32];
+    let mut _uart_a = pins::UartB::new(
+        peripherals.uart1_rx,
+        peripherals.uart1_tx,
+        &mut uart_1_rx_buf,
+        &mut uart_1_tx_buf,
+        uart_1_config,
+    )
+    .expect("UART1 should be present");
 }
--- src/pins.rs
+++ src/pins.rs
@@ -1,4 +1,22 @@
-use ariel_os::hal::peripherals;
+use ariel_os::hal::{peripherals, uart};
 
 #[cfg(context = "rp2040")]
-ariel_os::hal::define_peripherals!(Peripherals { btn: PIN_12 });
+pub type UartA<'a> = uart::UART0<'a>;
+
+#[cfg(context = "rp2040")]
+pub type UartB<'a> = uart::UART1<'a>;
+
+#[cfg(context = "rp2040")]
+ariel_os::hal::define_peripherals!(ButtonPeripherals { button: PIN_12 });
+
+#[cfg(context = "rp2040")]
+ariel_os::hal::define_peripherals!(UartAPeripherals {
+    uart0_tx: PIN_0,
+    uart0_rx: PIN_1,
+});
+
+#[cfg(context = "rp2040")]
+ariel_os::hal::define_peripherals!(UartBPeripherals {
+    uart1_tx: PIN_8,
+    uart1_rx: PIN_9
+});
